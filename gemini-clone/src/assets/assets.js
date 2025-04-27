002504defa0d2eb692a8898fa69070963bdb2223
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-27 21:13:55 +0300

    added assets

---
+++ gemini-clone/src/assets/assets.js
@@ -0,0 +1,25 @@
+import code_icon from './code.png'
+import compass_icon from './compass.png'
+import message_icon from './conversation.png'
+import gemini_icon from './gemini-color.png'
+import history_icon from './history.png'
+import gallery_icon from './image-gallery.png'
+import bulb_icon from './lightbulb.png'
+import menu_icon from './menu.png'
+import mic_icon from './mic.png'
+import plus_icon from './plus.png'
+import question_icon from './question.png'
+
+export const assets = {
+    code_icon,
+    compass_icon,
+    menu_icon,
+    message_icon,
+    gemini_icon,
+    bulb_icon,
+    gallery_icon,
+    history_icon,
+    mic_icon,
+    plus_icon,
+    question_icon
+}
