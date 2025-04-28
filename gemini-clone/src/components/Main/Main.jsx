2cc88ae1aba57701a72e67be6ecde78c92d81e30
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-28 12:22:46 +0300

    the response is displayed in the main page

--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -3,29 +3,38 @@ import { assets } from "../../assets/assets";
 import "./Main.css";
 import { Context } from "../../context/context";
 export default function Main() {
-  const {onSent} = useContext(Context);
-  const [input, setInput] = useState("")
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
 
-const handleSend = async ()=>{
-  if (!input.trim()) {
-    console.error("Input is empty. Please enter a query.");
-    return;
-  }
-  console.log("sending input:", input);
-  try{
-    const response = await onSent(input);
-    console.log("AI response: ", response);
-  }catch(error){
-    console.error("Error while sending: ",error);
-  }
-}
+  const handleSend = async () => {
+    if (!input.trim()) {
+      console.error("Input is empty. Please enter a query.");
+      return;
+    }
+    console.log("sending input:", input);
+    try {
+      const response = await onSent(input);
+      console.log("AI response: ", response);
+    } catch (error) {
+      console.error("Error while sending: ", error);
+    }
+  };
   return (
     <div className="main">
       <div className="nav">
         <p>Gemini</p>
       </div>
       <div className="main-container">
-        <div className="greet">
+        {!showResult?
+        <>
+<div className="greet">
           <p>
             <span>Hello, Dev</span>
           </p>
@@ -49,18 +58,40 @@ const handleSend = async ()=>{
             <img src={assets.code_icon} alt="" />
           </div>
         </div>
+        </>
+        :
+        <div className="result">
+          <div className="result-title">
+            <img src={assets.message_icon} alt="" />
+            <p>{recentPrompt}</p>
+          </div>
+          <div className="result-data">
+            <img src={assets.gemini_icon} alt="" />
+            <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+          </div>
+        </div>
+      }
+        
         <div className="main-bottom">
-            <div className="search-box">
-                <input type="text" value={input} onChange={(e)=>setInput(e.target.value)} placeholder="Ask something..." />
-                <div>
-                    <img src={assets.gallery_icon} alt="" />
-                    <img src={assets.mic_icon} alt="" />
-                    <button onClick={handleSend}> <img src={assets.send_icon} alt="" /></button>
-                </div>
+          <div className="search-box">
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Ask something..."
+            />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              
+                {" "}
+                <img src={assets.send_icon} alt=""onClick={handleSend} />
+             
             </div>
-            <p className="bottom-info">
-                Gemini may display inaccurate info, so double-check its responses.
-            </p>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, so double-check its responses.
+          </p>
         </div>
       </div>
     </div>
--- gemini-clone/src/context/context.jsx
+++ gemini-clone/src/context/context.jsx
@@ -1,16 +1,31 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { generateContent } from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+  const [newChat, setNewChat] = useState("");
+
   const onSent = async (contents) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
     if (!contents || !contents.trim()) {
-        console.error("Contents cannot be empty.");
-        throw new Error("Contents cannot be empty.");
-      }
+      console.error("Contents cannot be empty.");
+      throw new Error("Contents cannot be empty.");
+    }
     try {
       const response = await generateContent(contents);
+      setResultData(response);
+      setLoading(false);
+      setInput("");
       console.log("Generated content:", response);
       return response;
     } catch (error) {
@@ -20,7 +35,17 @@ const ContextProvider = (props) => {
   };
 
   const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
     onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+    newChat,
   };
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
