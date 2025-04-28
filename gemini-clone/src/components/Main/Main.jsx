c57ecf024ffa5cbf5f9d1ec0efe8fd5f529ab4e8
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-28 12:48:24 +0300

    added loader

--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -32,46 +32,54 @@ export default function Main() {
         <p>Gemini</p>
       </div>
       <div className="main-container">
-        {!showResult?
-        <>
-<div className="greet">
-          <p>
-            <span>Hello, Dev</span>
-          </p>
-          <p>How can I help you today?</p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest some beautiful places around Varna</p>
-            <img src={assets.compass_icon} alt="" />
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Dev</span>
+              </p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest some beautiful places around Varna</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Give me the greatest way to cook pasta</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>I want to plan my day including the following tasks...</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Improve the readability of the following code</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.message_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
           </div>
-          <div className="card">
-            <p>Give me the greatest way to cook pasta</p>
-            <img src={assets.bulb_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>I want to plan my day including the following tasks...</p>
-            <img src={assets.message_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Improve the readability of the following code</p>
-            <img src={assets.code_icon} alt="" />
-          </div>
-        </div>
-        </>
-        :
-        <div className="result">
-          <div className="result-title">
-            <img src={assets.message_icon} alt="" />
-            <p>{recentPrompt}</p>
-          </div>
-          <div className="result-data">
-            <img src={assets.gemini_icon} alt="" />
-            <p dangerouslySetInnerHTML={{__html:resultData}}></p>
-          </div>
-        </div>
-      }
-        
+        )}
+
         <div className="main-bottom">
           <div className="search-box">
             <input
@@ -82,11 +90,8 @@ export default function Main() {
             />
             <div>
               <img src={assets.gallery_icon} alt="" />
-              <img src={assets.mic_icon} alt="" />
-              
-                {" "}
-                <img src={assets.send_icon} alt=""onClick={handleSend} />
-             
+              <img src={assets.mic_icon} alt="" />{" "}
+              <img src={assets.send_icon} alt="" onClick={handleSend} />
             </div>
           </div>
           <p className="bottom-info">
